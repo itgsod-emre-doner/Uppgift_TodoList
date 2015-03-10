ed32663fbfc1843d7e24d328cfc4bed65a4324ea
itgsod-emre-doner <emre.doner@elev.it-gymnasiet.se>
2015-03-10 11:04:32 +0100

    Models
    Grejer händer

--- Todo/Models/models.py
+++ Todo/Models/models.py
@@ -1,7 +1,16 @@
 from pony.orm import PrimaryKey, Required, Set
 from Todo import db
 
+class Todo(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    text = Required(unicode)
+    tags = Set("Tag")
 
+
+class Tag(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    text = Required(unicode)
+    todos = Set(Todo)
 
 
 
